ab2b46c76f26aec4dc307f99c4cf041644054584
Mark Randles <mark.randles@bookit.com>
2014-05-01 13:39:14 -0400

    Removing legacy code completely

--- legacy/inc/prepend.php
+++
@@ -1,8 +0,0 @@
-<?php
-// currently not supported
-if(php_sapi_name() == 'cli')
-{
-	return;
-}
-
-xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
--- legacy/xhprof/includes/config.inc.sample.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	'url_base'   => 'https://dev.anuary.com/8d50658e-f8e0-5832-9e82-6b9e8aa940ac/',
-	'url_static' => NULL, // When undefined, it defaults to $config['url_base'] . 'public/'. This should be absolute URL.
-	'pdo'        => new PDO('mysql:dbname=your_database_name;host=localhost;charset=utf8', 'username', 'password'),
-);
