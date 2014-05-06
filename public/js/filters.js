3f8101cc1b3327eaa962216bd05953aa14eb39d7
Mark Randles <mark.randles@bookit.com>
2014-05-06 10:08:47 -0400

    better formatting

--- public/js/filters.js
+++ public/js/filters.js
@@ -20,5 +20,34 @@ appFilters.filter('formatTime', function () {
 	return function (raw) {
 		var ts = moment.unix(raw);
 		return ts.format('YYYY-MM-DD HH:mm:ss');
-	}
+	};
+});
+
+appFilters.filter('formatTiming', function () {
+	return function (timing) {
+		if (timing >= 1000000) {
+			return (timing / 1000000).toFixed(3) + 's';
+		} else if (timing >= 1000) {
+			return (timing / 1000).toFixed(3) + 'ms';
+		} else {
+			return timing.toString() + '\u00B5s';
+		}
+	};
+});
+
+appFilters.filter('formatMemory', function () {
+	return function (memory) {
+		var bins = ['B','K','M','G'];
+
+		for (var i=bins.length-1; i >= 0; i--) {
+			var cutoff = Math.pow(1024, i);
+			if (memory > cutoff) {
+				var val = memory / cutoff;
+				console.log(val);
+				return ((Number.isInteger(val)) ? val : val.toFixed(2)) + bins[i];
+			}
+		}
+
+		return memory;
+	};
 });
